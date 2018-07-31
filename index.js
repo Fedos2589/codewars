d82f4129f5bd2cd26af78b48d39798b6a224204f
Федор Громов <fedorgromovworkingbox@gmail.com>
2018-07-31 17:27:43 +0300

    another portion of tasks

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-// Your task is to sort a given string. Each word in the String will contain a single number. This number is the position the word should have in the result.
+// Your task is to sort a given string. Each word in the String will contain a single number.
+//This number is the position the word should have in the result.
 
 // Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
 
-// If the input String is empty, return an empty String. The words in the input String will only contain valid consecutive numbers.
+// If the input String is empty, return an empty String.
+//The words in the input String will only contain valid consecutive numbers.
 
 // For an input: "is2 Thi1s T4est 3a" the function should return "Thi1s is2 3a T4est"
 
@@ -60,7 +62,10 @@ let squareDigits = (num) =>
 
 squareDigits(9119)
 
-// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+// Given an array of integers, remove the smallest value.
+//Do not mutate the original array/list.
+//If there are multiple elements with the same value, remove the one with a lower index.
+//If you get an empty array/list, return an empty array/list.
 
 // Don't change the order of the elements that are left.
 
@@ -70,4 +75,116 @@ let removeSmallest = (numbers) =>
     .concat(numbers.slice(numbers.indexOf(Math.min.apply(Math, numbers)) + 1, numbers.length))
 
 removeSmallest([5,3,2,1,4])
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// a being 1, b being 2, etc.
+
+let alphabetPosition = (text) =>
+  text
+    .toLowerCase()
+    .replace(/[^a-z]/g, '')
+    .split('')
+    .map(symbol => symbol.charCodeAt(0) - 96)
+    .join(' ')
+
+// The new "Avengers" movie has just been released!
+// There are a lot of people at the cinema box office standing in a huge line.
+// Each of them has a single 100, 50 or 25 dollars bill. An "Avengers" ticket costs 25 dollars.
+
+// Vasya is currently working as a clerk.
+// He wants to sell a ticket to every single person in this line.
+
+// Can Vasya sell a ticket to each person and give the change if he initially has no money and sells the tickets strictly in the order people follow in the line?
+
+// Return YES, if Vasya can sell a ticket to each person and give the change with the bills he has at hand at that moment. Otherwise return NO.
+
+let tickets = (peopleInLine) => {
+  const answers = {
+    true: 'YES',
+    false: 'NO'
+  }
+
+  let bills25 = 0
+  let bills50 = 0
+
+  let res = peopleInLine.reduce((acc, cur) => {
+    switch(cur) {
+      case 25:
+        bills25++
+        return true
+      case 50:
+        if (bills25) {
+          bills25--
+          bills50++
+          return true
+        } else {
+          return false
+        }
+      case 100:
+        if (bills25 && bills50) {
+          bills25--
+          bills50--
+          return true
+        } else if (bills25 > 2) {
+          bills25 = bills25 - 3
+          return true
+        } else {
+          return false
+        }
+    }
+  }, true)
+
+  return answers[res]
+}
+
+tickets([25,25,50,100,25,50,25,100])
+
+// Given two arrays of strings a1 and a2 return a sorted array r in lexicographical
+// order of the strings of a1 which are substrings of strings of a2.
+
+// #Example 1: a1 = ["arp", "live", "strong"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns ["arp", "live", "strong"]
+
+// #Example 2: a1 = ["tarp", "mice", "bull"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns []
+
+// Notes:
+// Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+
+// In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+
+// Beware: r must be without duplicates.
+// Don't mutate the inputs.
+
+let inArray = (arr1, arr2) =>
+  arr1
+    .filter(item => arr2.find(targetItem => targetItem.includes(item)))
+    .sort()
+
+inArray(["xyz", "live", "strong"], ["lively", "alive", "harp", "sharp", "armstrong"])
+
+// Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+// anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+// anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+// anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+
+let anagrams = (word, words) =>
+  words
+    .filter(curWord => curWord.split('').every(letter => word.includes(letter)))
+    .filter(curWord => word.split('').every(letter => curWord.includes(letter)))
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])
+
 
