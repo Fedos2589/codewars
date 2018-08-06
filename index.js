bc36f8813accddf126f4223564a8bbb987d6a3b3
Федор Громов <fedorgromovworkingbox@gmail.com>
2018-08-06 19:17:43 +0300

    another task

--- index.js
+++ index.js
@@ -236,9 +236,114 @@ let titleCase = (target, exceptions) => {
 
 titleCase('THE WIND IN THE WILLOWS', 'The In')
 
+// Complete the solution so that it splits the string into pairs of two characters.
+// If the string contains an odd number of characters then it should replace the missing second
+// character of the final pair with an underscore ('_').
+
+let splitTwo = (str) => {
+  str = str.length % 2 !== 0 ? str + '_' : str
+  return str.replace(/(\w\w)/g, '$1 ').trim()
+}
+
+// Given an array, find the int that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+let findOdd = (arr) => {
+  let counter = 0
+  for (let i in arr) {
+    if (arr[i] === arr[0]) {
+      counter++
+    }
+  }
+  return counter % 2 !== 0 ? arr[0] : findOdd(arr.filter(item => item !== arr[0]))
+}
+ 
+// Divisors of 42 are : 1, 2, 3, 6, 7, 14, 21, 42. These divisors squared are: 1, 4, 9, 36, 49, 196, 441, 1764. The sum of the squared divisors is 2500 which is 50 * 50, a square!
+
+// Given two integers m, n (1 <= m <= n) we want to find all integers between m and n whose sum of squared divisors is itself a square. 42 is such a number.
+
+// The result will be an array of arrays or of tuples (in C an array of Pair) or a string, each subarray having two elements, first the number whose squared divisors is a square and then the sum of the squared divisors.
+
+// #Examples:
+
+// list_squared(1, 250) --> [[1, 1], [42, 2500], [246, 84100]]
+// list_squared(42, 250) --> [[42, 2500], [246, 84100]]
 
 
+let listSquared = (m, n) => {
+  let getAllDevisors = (number) => {
+    let devisorsArray = []
 
+    for (let i = 1; i <= number; i++) {
+      if (number % i === 0) devisorsArray.push(i)
+    }
+
+    return devisorsArray
+  }
+
+  let squaredDevisorsSum = (arr) => arr.map(item => item * item).reduce((acc, cur) => acc + cur)
+
+  let result = []
+
+  for (let i = m; i <= n; i++) {
+    if (Number.isInteger(Math.sqrt(squaredDevisorsSum(getAllDevisors(i))))){
+      result.push([i, squaredDevisorsSum(getAllDevisors(i))])
+    }
+  }
+
+  return result
+}
+
+// Alright, detective, one of our colleagues successfully observed our target person, Robby the robber.
+// We followed him to a secret warehouse, where we assume to find all the stolen stuff.
+// The door to this warehouse is secured by an electronic combination lock.
+// Unfortunately our spy isn't sure about the PIN he saw, when Robby entered it.
+
+// The keypad has the following layout:
+
+// ┌───┬───┬───┐
+// │ 1 │ 2 │ 3 │
+// ├───┼───┼───┤
+// │ 4 │ 5 │ 6 │
+// ├───┼───┼───┤
+// │ 7 │ 8 │ 9 │
+// └───┼───┼───┘
+//     │ 0 │
+//     └───┘
+// He noted the PIN 1357, but he also said, it is possible that each of the digits he saw could actually be another adjacent digit (horizontally or vertically, but not diagonally). 
+// E.g. instead of the 1 it could also be the 2 or 4.
+// And instead of the 5 it could also be the 2, 4, 6 or 8.
+
+// He also mentioned, he knows this kind of locks.
+// You can enter an unlimited amount of wrong PINs, they never finally lock the system or sound the alarm.
+// That's why we can try out all possible (*) variations.
+
+// * possible in sense of: the observed PIN itself and all variations considering the adjacent digits
+
+// Can you help us to find all those variations?
+// It would be nice to have a function, that returns an array (or a list in Java and C#) of all variations for an observed PIN with a length of 1 to 8 digits.
+// We could name the function getPINs (get_pins in python, GetPINs in C#).
+// But please note that all PINs, the observed one and also the results, must be strings, because of potentially leading '0's.
+// We already prepared some test cases for you.
+
+// Detective, we count on you!
+
+let getPINs = (observed) => {
+  const adjacents = {
+    1: ['1', '2', '4'],
+    2: ['1', '2', '3', '5'],
+    3: ['2', '3', '6'],
+    4: ['1', '4', '5'],
+    5: ['2', '4', '5', '6', '8'],
+    6: ['3', '5'. '6', '9'],
+    7: ['4', '7', '8'],
+    8: ['5', '7', '8', '9', '0'],
+    9: ['6', '8', '9'],
+    0: ['8', '0']
+  }
+  
+}
 
 
 
