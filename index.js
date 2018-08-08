60390a4185b89380606e12b6c199cf4b3c6398cb
Федор Громов <fedorgromovworkingbox@gmail.com>
2018-08-08 11:12:12 +0300

    description

--- index.js
+++ index.js
@@ -347,9 +347,27 @@ let isPP = (n) => {
   return null
 }
 
+// The marketing team are spending way too much time typing in hashtags.
+// Let's help them with out own Hashtag Generator!
 
+// Here's the deal:
 
+// If the final result is longer than 140 chars it must return false.
+// If the input is a empty string it must return false.
+// It must start with a hashtag (#).
+// All words must have their first letter capitalized.
+// Example Input to Output:
 
+// " Hello there thanks for trying my Kata" => "#HelloThereThanksForTryingMyKata"
+
+// " Hello World " => "#HelloWorld"
+
+let generateHashtag = (str) =>
+  '#' + str
+    .trim()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.substr(1))
+    .join('')
 
 
 
