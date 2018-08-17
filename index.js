ca867b9bac7b58e6a5cab5b61e80c095cf207fd5
Федор Громов <fedorgromovworkingbox@gmail.com>
2018-08-17 10:59:28 +0300

    songDecoder

--- index.js
+++ index.js
@@ -617,6 +617,82 @@ let persistence = (num) => {
   return counter
 }
 
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+// The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
+// Write a method that takes the array as an argument and returns this "outlier" N.
+
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+let findOutlier = (integers) => {
+  let oddArray = (arr) => arr.filter(item => item % 2 !== 0).length > 1
+
+
+  return oddArray(integers)
+    ? integers.find(item => item % 2 === 0)
+    : integers.find(item => item % 2 !== 0)
+}
+
+// Write a function that takes in a string of one or more words, and returns the same string,
+// but with all five or more letter words reversed (Just like the name of this Kata).
+// Strings passed in will consist of only letters and spaces.
+// Spaces will be included only when more than one word is present.
+
+
+// Examples:
+
+// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+// spinWords( "This is a test") => returns "This is a test" 
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+
+let spinWords = (str) =>
+  str
+    .split(' ')
+    .map(word => word.length > 4 ? word.split('').reverse().join('') : word).join(' ')
+
+// Polycarpus works as a DJ in the best Berland nightclub, and he often uses dubstep music in his performance.
+// Recently, he has decided to take a couple of old songs and make dubstep remixes from them.
+
+// Let's assume that a song consists of some number of words.
+// To make the dubstep remix of this song, Polycarpus inserts a certain number of words "WUB" before the 
+// first word of the song (the number may be zero), after the last word (the number may be zero),
+// and between words (at least one between any pair of neighbouring words),
+// and then the boy glues together all the words, including "WUB", in one string and plays the song at the club.
+
+// For example, a song with words "I AM X" can transform into a dubstep remix as "WUBWUBIWUBAMWUBWUBX" and cannot transform into "WUBWUBIAMWUBX".
+
+// Recently, Jonny has heard Polycarpus's new dubstep track, but since he isn't into modern music, he decided to find out what was the initial song that Polycarpus remixed.
+// Help Jonny restore the original song.
+
+// Input
+// The input consists of a single non-empty string, consisting only of uppercase English letters, the string's length doesn't exceed 200 characters
+
+// Output
+// Return the words of the initial song that Polycarpus used to make a dubsteb remix.
+// Separate the words with a space.
+
+// Examples
+// songDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB")
+//   // =>  WE ARE THE CHAMPIONS MY FRIEND
+
+let songDecoder = (song) => song.replace(/(WUB){1,}/g, ' ').trim()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
