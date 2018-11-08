86dd99e6beaefe67eab5a6db2fa714f951f01dcf
Федор Громов <fedorgromovworkingbox@gmail.com>
2018-11-08 17:45:26 +0300

    towerBuilder

--- index.js
+++ index.js
@@ -1206,8 +1206,40 @@ let findMissingLetter = (array) =>
 let comp = (arr1, arr2) =>
   arr1 !== null && arr2 !== null && !arr2.filter(item => !arr1.includes(Math.sqrt(item))).length
 
+// Build Tower
+// Build Tower by the following given argument:
+// number of floors (integer and always greater than 0).
 
+// Tower block is represented as *
 
+// Python: return a list;
+// JavaScript: returns an Array;
+// C#: returns a string[];
+// PHP: returns an array;
+// C++: returns a vector<string>;
+// Haskell: returns a [String];
+// Ruby: returns an Array;
+// Have fun!
+
+// for example, a tower of 3 floors looks like below
+
+// [
+//   '  *  ', 
+//   ' *** ', 
+//   '*****'
+// ]
+
+let towerBuilder = (nFloors) => {
+  let result = []
+  let len = nFloors * 2 -1
+  for (let i = 1; i <= nFloors; i++) {
+    let numberOfStars = i * 2 - 1
+    let numberOfSpaces = (len - numberOfStars) / 2
+    result.push(`${' '.repeat(numberOfSpaces).concat('*'.repeat(numberOfStars)).concat(' '.repeat(numberOfSpaces))}`)
+  }
+
+  return result
+}
 
 
 
